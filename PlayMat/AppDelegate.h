0d20769c343d368450383e296d039849bd78c91c
Kasabime <p1j98x1i@gmail.com>
2015-03-07 17:29:35 +0900

    Initial Commit

---
+++ PlayMat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlayMat
+//
+//  Created by lion on 2015/03/07.
+//  Copyright (c) 2015年 Kasabime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlayMat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlayMat
+//
+//  Created by lion on 2015/03/07.
+//  Copyright (c) 2015年 Kasabime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
